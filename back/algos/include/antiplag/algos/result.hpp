40398c22004781ffa515ff635b979d4304684674
Timoha <makarshint@gmail.com>
2026-06-23 16:55:50 +0300

    add comments

--- back/algos/include/antiplag/algos/result.hpp
+++ back/algos/include/antiplag/algos/result.hpp
@@ -23,11 +23,19 @@ struct MatchFragment {
 
 struct DetectionResult {
     std::string algorithm;
-    double score = 0.0;
-    double containment = 0.0;
+    double score = 0.0; // основной коэфф схожести [0;1], где 0 - не похоже, 1 - почти одинаково
+    double containment = 0.0; // показывает, насколько один файл содержится в другом (копипаст маленьких кусков кода)
     std::vector<MatchFragment> fragments;
     std::map<std::string, std::string> parameters;
-    std::map<std::string, double> component_scores;
+    std::map<std::string, double> component_scores; // оценки отдельных алгосов 
+/*
+    component_scores["token_ngram"]
+    component_scores["winnowing"]
+    component_scores["greedy_string_tiling"]
+    component_scores["structural_skeleton"]
+
+    score = sum(component_scores[i]{i = "algos_name"}) / n (number of algos)
+*/
     std::vector<std::string> warnings;
 };
 
